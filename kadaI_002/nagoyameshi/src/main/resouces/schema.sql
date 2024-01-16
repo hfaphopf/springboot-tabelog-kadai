2071db8ca2f6950ee4e7b646fade2e21704f0bd8
hfaphopf <kuma.shou0925@gmail.com>
2024-01-16 12:25:25 +0900

    Update schema.sql

--- kadaI_002/nagoyameshi/src/main/resouces/schema.sql
+++ kadaI_002/nagoyameshi/src/main/resouces/schema.sql
@@ -8,9 +8,9 @@ CREATE TABLE IF NOT EXISTS restaurants (
      postal_code VARCHAR(50) NOT NULL,
      address VARCHAR(255) NOT NULL,
      phone_number VARCHAR(50) NOT NULL,
-     category INT NOT NULL,
-     regular holiday INT NOT NULL,
-     business hours VARCHAR(50) NOT NULL,
+     category VARCHAR(50) NOT NULL,
+     regular_holiday VARCHAR(50) NOT NULL,
+     business_hours VARCHAR(50) NOT NULL,
      created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
      updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
  );
