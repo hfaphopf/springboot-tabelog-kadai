3de29bad57036426b8cc118443901040c4e0ec6d
hfaphopf <kuma.shou0925@gmail.com>
2024-01-26 07:32:46 +0900

    Update VerificationTokenRepository.java

--- kadaI_002/nagoyameshi/src/main/Java/com/example/nagoyameshi/repository/VerificationTokenRepository.java
+++ kadaI_002/nagoyameshi/src/main/Java/com/example/nagoyameshi/repository/VerificationTokenRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.nagoyameshi.entity.VerificationToken;
 
 public interface VerificationTokenRepository extends JpaRepository< VerificationToken, Integer>{
-	public VerificationToken findByToken(String Token);
+	public VerificationToken findByToken(String token);
 }
