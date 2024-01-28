4639719de8d48333fb34c36442c689f1b99d7e96
hfaphopf <kuma.shou0925@gmail.com>
2024-01-28 20:07:38 +0900

    Update RestaurantRepository.java

--- kadaI_002/nagoyameshi/src/main/Java/com/example/nagoyameshi/repository/RestaurantRepository.java
+++ kadaI_002/nagoyameshi/src/main/Java/com/example/nagoyameshi/repository/RestaurantRepository.java
@@ -8,8 +8,13 @@ import com.example.nagoyameshi.entity.Restaurant;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
      public Page<Restaurant> findByNameLike(String keyword, Pageable pageable);
-
-     public Page<Restaurant> findByNameLikeOrAddressLike(String nameKeyword, String addressKeyword, Pageable pageable);    
-     public Page<Restaurant> findByAddressLike(String area, Pageable pageable);
-     public Page<Restaurant> findByPriceLessThanEqual(Integer price, Pageable pageable);
+     
+     public Page<Restaurant> findByNameLikeOrAddressLikeOrderByCreatedAtDesc(String nameKeyword, String addressKeyword, Pageable pageable);  
+     public Page<Restaurant> findByNameLikeOrAddressLikeOrderByPriceAsc(String nameKeyword, String addressKeyword, Pageable pageable);  
+     public Page<Restaurant> findByAddressLikeOrderByCreatedAtDesc(String genre, Pageable pageable);
+     public Page<Restaurant> findByAddressLikeOrderByPriceAsc(String genre, Pageable pageable);
+     public Page<Restaurant> findByPriceLessThanEqualOrderByCreatedAtDesc(Integer price, Pageable pageable);
+     public Page<Restaurant> findByPriceLessThanEqualOrderByPriceAsc(Integer price, Pageable pageable); 
+     public Page<Restaurant> findAllByOrderByCreatedAtDesc(Pageable pageable);
+     public Page<Restaurant> findAllByOrderByPriceAsc(Pageable pageable);   
 }
