63799ac0c04dc40f6c807bd73fa9018e77decc6e
hfaphopf <kuma.shou0925@gmail.com>
2024-02-08 19:32:38 +0900

    Update ReservationService.java

--- kadaI_002/nagoyameshi/src/main/Java/com/example/nagoyameshi/service/ReservationService.java
+++ kadaI_002/nagoyameshi/src/main/Java/com/example/nagoyameshi/service/ReservationService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class ReservationService {
 	// 宿泊人数が定員以下かどうかをチェックする
-	public boolean isWithinCapacity(Integer numberOfPeople, Integer seats) {
-        return numberOfPeople <= seats;
-    }
+	public boolean isWithinSeats(Integer numberOfPeople, Integer seats) {
+		return numberOfPeople <= seats;
+	}
 }
