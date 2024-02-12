cef5a2b6f915c18b4a5528aebada6acf97ebc70e
hfaphopf <kuma.shou0925@gmail.com>
2024-02-12 13:47:43 +0900

    Update RestaurantRegisterForm.java

--- kadaI_002/nagoyameshi/src/main/Java/com/example/nagoyameshi/form/RestaurantRegisterForm.java
+++ kadaI_002/nagoyameshi/src/main/Java/com/example/nagoyameshi/form/RestaurantRegisterForm.java
@@ -36,7 +36,7 @@ public class RestaurantRegisterForm {
 	@NotBlank(message = "定休日を入力してください。")
 	private String regularHoliday;
 
-	@NotBlank(message = "座席数を入力してください。")
+	@NotNull(message = "座席数を入力してください。")
 	private Integer seats;
 
 	@NotBlank(message = "カテゴリを入力してください。")
