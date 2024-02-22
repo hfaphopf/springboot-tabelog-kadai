50aa0ed79965a88bf81bd67becde2569a0a9f1aa
hfaphopf <kuma.shou0925@gmail.com>
2024-02-22 13:25:20 +0900

    Update UserController.java

--- kadaI_002/nagoyameshi/src/main/Java/com/example/nagoyameshi/controller/UserController.java
+++ kadaI_002/nagoyameshi/src/main/Java/com/example/nagoyameshi/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -79,7 +80,16 @@ public class UserController {
 		return "user/changepaid";
 	}
 	
-	
+	@PostMapping("/{id}/paidupdate")
+    public String paidupdate(@PathVariable(name = "id") @ModelAttribute @Validated BindingResult bindingResult, RedirectAttributes redirectAttributes) {        
+        if (bindingResult.hasErrors()) {
+            return "user/changepaid";
+        }
+        
+        redirectAttributes.addFlashAttribute("successMessage", "ステータスを有料会員に変更しました。");
+        
+        return "redirect:/admin/users";
+    }
 	
 	@GetMapping("/company")
 	public String company() {
