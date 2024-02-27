b826083a2954b0a165318b2b9166ca77045d6f3b
hfaphopf <kuma.shou0925@gmail.com>
2024-02-27 18:42:08 +0900

    Update UserRepository.java

--- kadaI_002/nagoyameshi/src/main/Java/com/example/nagoyameshi/repository/UserRepository.java
+++ kadaI_002/nagoyameshi/src/main/Java/com/example/nagoyameshi/repository/UserRepository.java
@@ -7,8 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.nagoyameshi.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-	public User findByEmail(String email);
-	public User findById(int id);
+	public User findUserByEmail(String email);
+	//idを指定してuserを取得する
+	public User findUserById(int id);
 
 	public Page<User> findByNameLikeOrFuriganaLike(String nameKeyword, String furiganaKeyword, Pageable pageable);
 }
