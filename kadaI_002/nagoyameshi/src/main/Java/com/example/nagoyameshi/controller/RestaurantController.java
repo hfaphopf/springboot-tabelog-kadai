069d6069c0f0adce8cca0ad3b3a87f0b2e282524
hfaphopf <kuma.shou0925@gmail.com>
2024-03-01 16:11:29 +0900

    Update RestaurantController.java

--- kadaI_002/nagoyameshi/src/main/Java/com/example/nagoyameshi/controller/RestaurantController.java
+++ kadaI_002/nagoyameshi/src/main/Java/com/example/nagoyameshi/controller/RestaurantController.java
@@ -1,9 +1,12 @@
 package com.example.nagoyameshi.controller;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,16 +15,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.nagoyameshi.entity.Restaurant;
+import com.example.nagoyameshi.entity.Review;
+import com.example.nagoyameshi.entity.User;
 import com.example.nagoyameshi.form.ReservationInputForm;
 import com.example.nagoyameshi.repository.RestaurantRepository;
+import com.example.nagoyameshi.repository.ReviewRepository;
+import com.example.nagoyameshi.security.UserDetailsImpl;
+import com.example.nagoyameshi.service.ReviewService;
 
 @Controller
 @RequestMapping("/restaurants")
 public class RestaurantController {
 	private final RestaurantRepository restaurantRepository;
+	private final ReviewRepository reviewRepository;
+	private final ReviewService reviewService;
 
-	public RestaurantController(RestaurantRepository restaurantRepository) {
+	public RestaurantController(RestaurantRepository restaurantRepository, ReviewRepository reviewRepository,
+			ReviewService reviewService) {
 		this.restaurantRepository = restaurantRepository;
+		this.reviewRepository = reviewRepository;
+		this.reviewService = reviewService;
 	}
 
 	@GetMapping
@@ -72,11 +85,25 @@ public class RestaurantController {
 	}
 
 	@GetMapping("/{id}")
-	public String show(@PathVariable(name = "id") Integer id, Model model) {
+	public String show(@PathVariable(name = "id") Integer id, Model model,  @AuthenticationPrincipal UserDetailsImpl userDetailsImpl) {
 		Restaurant restaurant = restaurantRepository.getReferenceById(id);
+
+		boolean hasUserAlreadyReviewed = false;
+
+		if (userDetailsImpl != null) {
+			User user = userDetailsImpl.getUser();
+			hasUserAlreadyReviewed = reviewService.hasUserAlreadyReviewed(restaurant, user);
+
+		}
+
+		List<Review> newReviews = reviewRepository.findTop6ByRestaurantOrderByCreatedAtDesc(restaurant);
+		long totalReviewCount = reviewRepository.countByRestaurant(restaurant);
 
 		model.addAttribute("restaurant", restaurant);
 		model.addAttribute("reservationInputForm", new ReservationInputForm());
+		model.addAttribute("hasUserAlreadyReviewed", hasUserAlreadyReviewed);
+		model.addAttribute("newReviews", newReviews);
+		model.addAttribute("totalReviewCount", totalReviewCount);
 
 		return "restaurants/show";
 	}
