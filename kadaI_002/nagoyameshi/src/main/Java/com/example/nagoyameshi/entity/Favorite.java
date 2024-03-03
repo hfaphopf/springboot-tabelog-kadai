94884698036ab5c703433b7193a31f5615d28c79
hfaphopf <kuma.shou0925@gmail.com>
2024-03-03 15:05:52 +0900

    Update Favorite.java

--- kadaI_002/nagoyameshi/src/main/Java/com/example/nagoyameshi/entity/Favorite.java
+++ kadaI_002/nagoyameshi/src/main/Java/com/example/nagoyameshi/entity/Favorite.java
@@ -12,26 +12,26 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
-@Entity	
-@Table(name = "favorites")	
+@Entity
+@Table(name = "favorites")
 @Data
 public class Favorite {
-	@Id	
-    @GeneratedValue(strategy = GenerationType.IDENTITY)	
-    @Column(name = "id")	
-    private Integer id;	
-    	
-    @ManyToOne	
-    @JoinColumn(name = "restaurant_id")	
-    private Restaurant restaurant; 	
-    	
-    @ManyToOne	
-    @JoinColumn(name = "user_id")	
-    private User user;	
-    	
-    @Column(name = "created_at", insertable = false, updatable = false)	
-    private Timestamp createdAt;	
-    	
-    @Column(name = "updated_at", insertable = false, updatable = false)	
-    private Timestamp updatedAt;    	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+
+	@ManyToOne
+	@JoinColumn(name = "restaurant_id")
+	private Restaurant restaurant;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@Column(name = "created_at", insertable = false, updatable = false)
+	private Timestamp createdAt;
+
+	@Column(name = "updated_at", insertable = false, updatable = false)
+	private Timestamp updatedAt;
 }
