79ffc1417aa07759b6ae5c6c92a8cc8ab7d5ab61
hfaphopf <kuma.shou0925@gmail.com>
2024-03-03 15:28:49 +0900

    Update UserDetailsServiceImpl.java

--- kadaI_002/nagoyameshi/src/main/Java/com/example/nagoyameshi/security/UserDetailsServiceImpl.java
+++ kadaI_002/nagoyameshi/src/main/Java/com/example/nagoyameshi/security/UserDetailsServiceImpl.java
@@ -14,23 +14,23 @@ import com.example.nagoyameshi.entity.User;
 import com.example.nagoyameshi.repository.UserRepository;
 
 @Service
-public class UserDetailsServiceImpl implements UserDetailsService{
+public class UserDetailsServiceImpl implements UserDetailsService {
 	private final UserRepository userRepository;
-	
-	public UserDetailsServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;        
-    }
 
-	 @Override
-     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {  
-         try {
-             User user = userRepository.findByEmail(email);
-             String userRoleName = user.getRole().getName();
-             Collection<GrantedAuthority> authorities = new ArrayList<>();         
-             authorities.add(new SimpleGrantedAuthority(userRoleName));
-             return new UserDetailsImpl(user, authorities);
-         } catch (Exception e) {
-             throw new UsernameNotFoundException("ユーザーが見つかりませんでした。");
-         }
-     }
+	public UserDetailsServiceImpl(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		try {
+			User user = userRepository.findUserByEmail(email);
+			String userRoleName = user.getRole().getName();
+			Collection<GrantedAuthority> authorities = new ArrayList<>();
+			authorities.add(new SimpleGrantedAuthority(userRoleName));
+			return new UserDetailsImpl(user, authorities);
+		} catch (Exception e) {
+			throw new UsernameNotFoundException("ユーザーが見つかりませんでした。");
+		}
+	}
 }
