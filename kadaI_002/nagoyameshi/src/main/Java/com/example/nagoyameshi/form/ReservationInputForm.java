6b9f3d11edd3862828919b69d147f151752cdea7
hfaphopf <kuma.shou0925@gmail.com>
2024-03-03 15:10:17 +0900

    Update ReservationInputForm.java

--- kadaI_002/nagoyameshi/src/main/Java/com/example/nagoyameshi/form/ReservationInputForm.java
+++ kadaI_002/nagoyameshi/src/main/Java/com/example/nagoyameshi/form/ReservationInputForm.java
@@ -10,26 +10,26 @@ import lombok.Data;
 @Data
 public class ReservationInputForm {
 	@NotBlank(message = "予約日を選択してください。")
-    private String reservationDate;    
-    
-    @NotNull(message = "人数を入力してください。")
-    @Min(value = 1, message = "人数は1人以上に設定してください。")
-    private Integer numberOfPeople; 
-    
- // 予約日を取得する
-    
-    //フィールド
-    private LocalDate checkinDate;
-    
-    //ゲッター
-    public LocalDate getCheckinDate() {
-//        String checkReservationDate = getReservationDate();
-//        return LocalDate.parse(checkReservationDate);
-    	return this.checkinDate;
-    }
-    
-    //セッター
-    public void setCheckinDate(LocalDate checkinDate) {
-    	this.checkinDate = checkinDate;
-    }
+	private String reservationDate;
+
+	@NotNull(message = "人数を入力してください。")
+	@Min(value = 1, message = "人数は1人以上に設定してください。")
+	private Integer numberOfPeople;
+
+	// 予約日を取得する
+
+	//フィールド
+	private LocalDate checkinDate;
+
+	//ゲッター
+	public LocalDate getCheckinDate() {
+		//        String checkReservationDate = getReservationDate();
+		//        return LocalDate.parse(checkReservationDate);
+		return this.checkinDate;
+	}
+
+	//セッター
+	public void setCheckinDate(LocalDate checkinDate) {
+		this.checkinDate = checkinDate;
+	}
 }
