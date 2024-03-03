e5a7113607839077e357488055da6038a562013a
hfaphopf <kuma.shou0925@gmail.com>
2024-03-03 15:02:15 +0900

    Update Companycontroller.java

--- kadaI_002/nagoyameshi/src/main/Java/com/example/nagoyameshi/controller/Companycontroller.java
+++ kadaI_002/nagoyameshi/src/main/Java/com/example/nagoyameshi/controller/Companycontroller.java
@@ -1,43 +1,13 @@
 package com.example.nagoyameshi.controller;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import com.example.nagoyameshi.form.CompanyEditForm;
-import com.example.nagoyameshi.repository.UserRepository;
-import com.example.nagoyameshi.security.UserDetailsImpl;
 
 @Controller
-public class Companycontroller {
-	
+public class CompanyController {
+
 	@GetMapping("/company")
-    public String company() {
-    return "admin/company/CompanyPage";
-    }
-
-	@GetMapping("/companyedit")
-	public String edit(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl, Model model) {        
-	    User user = userRepository.getReferenceById(userDetailsImpl.getUser().getId());  
-	    CompanyEditForm companyEditForm = new CompanyEditForm(user.getCompanyName(), user.getLocation(), user.getRepresentativeName(), user.getEstablishmentDate(), user.getCapital(), user.getPhoneNumber(), user.getBusinessContent());
-	    model.addAttribute("companyEditForm", companyEditForm);
-	    
-	    return "user/companyedit";
+	public String company() {
+		return "admin/company/CompanyPage";
 	}
-	
-	@PostMapping("/companyedit")
-	public String companyEdit(@ModelAttribute CompanyEditForm companyEditForm) {
-	    // 会社情報の編集処理
-	    
-	    return "redirect:/company"; // 編集後に会社情報ページにリダイレクト
-	}
-	
-	private final UserRepository userRepository;
-
-    public Companycontroller(UserRepository userRepository) {
-        this.userRepository = userRepository;            
-    }
 }
